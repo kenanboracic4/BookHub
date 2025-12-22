ae2c5f02d87da4d404be7171ebb16639fcb961df
kenanboracic4 <kenanboracic.dev@gmail.com>
2025-12-22 16:20:53 +0100

    Fix LK data

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -26,5 +26,27 @@ module.exports = {
        res.render('addBook', {
             ...LKData
        });
+    },
+
+    async handleAddBook(req, res) {
+       try{
+              const bookData= {
+                     title: req.body.title,
+                     author: req.body.author,
+                     description: req.body.description,
+                     price: parseFloat(req.body.price) || 0.00,
+                     imageUrl: req.file ? `/uploads/${req.file.filename}` : null,
+                     genreId: req.body.genreId || null,
+                     locationId: req.body.locationId || null,
+                     conditionId: req.body.conditionId || null,
+                     languageId: req.body.languageId || null
+              };
+              await bookService.addBook(bookData);
+              
+              res.redirect('/books?success=true');
+       } catch (error) {
+              console.error('Greška prilikom dodavanja knjige:', error);
+              res.status(500).send('Došlo je do greške prilikom dodavanja knjige.');
+       }
     }
 };
